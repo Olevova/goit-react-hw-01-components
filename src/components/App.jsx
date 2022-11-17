4eda46cf046054ed03370c8ecd73c8487d0b7b5c
Olevova <olevova1983@gmail.com>
2022-11-18 00:05:27 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import {Profile} from "./Profile/Profile";
+import user from 'user.json';
+// const RT = ({ name, email }) =>
+// (<div>
+//   <h1>{name}</h1>
+//   <p>{email}</p>
+// </div>);
+  
+console.log(user);
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div>
+    <Profile avatar={user.avatar}
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      stats={user.stats}
+    />;
+    </div>  
 };
+  
+  //  <div style={{
+  //   height: '100vh',
+  //   display: 'flex',
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   fontSize: 40,
+  //   color: '#010101'
+  // }} ></div>
+
+  
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,91 @@
+import PropTypes from "prop-types";
+import css from "./Profile.module.css";
+
+
+// function SocialCard ({ name, tag, location, avatar, stats }) {
+ 
+//   return (
+//     <div className={css.profile}>
+//       <div className={css.description}>
+//         <img src={avatar} alt="user avatar" className={css.avatar} />
+//         <p className={css.name}>{name}</p>
+//         <p className={css.tag}>@{tag}</p>
+//         <p className={css.location}>{location}</p>
+//       </div>
+
+//       <ul className={css.stats}>
+//         <li>
+//           <span className={css.label}>Followers</span>
+//          <span className={css.quantity}>{stats.followers}</span>
+//         </li>
+//         <li>
+//           <span className={css.label}>Views</span>
+//           <span className={css.quantity}>{stats.views}</span>
+//         </li>
+//         <li>
+//           <span className={css.label}>Likes</span>
+//           <span className={css.quantity}>{stats.likes}</span>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// };
+
+// SocialCard.propTypes = {
+//   user: PropTypes.shape({
+//     name: PropTypes.string.isRequired,
+//     tag: PropTypes.string.isRequired,
+//     location: PropTypes.string.isRequired,
+//     avatar: PropTypes.string.isRequired,
+//     stats: PropTypes.shape({
+//       followers: PropTypes.number.isRequired,
+//       views: PropTypes.number.isRequired,
+//       likes: PropTypes.number.isRequired,
+//     }).isRequired,
+//   }).isRequired,
+// };
+
+// export default SocialCard;
+
+
+
+export const Profile = ({username, tag, location, avatar, stats}) => {
+    console.log({username, tag, location, avatar, stats});
+    return <div className={css.profile}>
+            <div className="description">
+                <img
+                    src={avatar}
+                    alt="User avatar"
+                    className="avatar"
+                />
+                <p className="name">{username}</p>
+                <p className="tag">{"@" + tag}</p>
+                <p className="location">{location}</p>
+            </div>
+
+            <ul className="stats">
+                <li>
+                    <span className="label">Followers</span>
+                    <span className="quantity">{stats.followers}</span>
+                </li>
+                <li>
+                    <span className="label">Views</span>
+                    <span className="quantity">{stats.vievs}</span>
+                </li>
+                <li>
+                    <span className="label">Likes</span>
+                    <span className="quantity">{stats.likes}</span>
+                </li>
+            </ul>
+        </div>
+}
+
+Profile.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired
+}
